0fd1a71b2fae18faa3c6db49b32c6b134e19fd96
mostafa hosseini <mostafa.hosseini.db@gmail.com>
2019-02-25 10:48:32 +0330

    Updates

---
+++ precache-manifest.c78315e060dcc32758cdf65a5653ecb2.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "75bbd3d2379271601fdc3114c5bec64a",
+    "url": "/test/static/media/robotobold.75bbd3d2.woff"
+  },
+  {
+    "revision": "96b3b238a09b63fbbc09",
+    "url": "/test/static/css/main.8d7d1c49.chunk.css"
+  },
+  {
+    "revision": "a03dc12cda14a16444a4",
+    "url": "/test/static/js/runtime~main.a03dc12c.js"
+  },
+  {
+    "revision": "cb67ac95af72b1983efb",
+    "url": "/test/static/js/2.cb67ac95.chunk.js"
+  },
+  {
+    "revision": "ccabf309497698464842397f1319b345",
+    "url": "/test/static/media/logo.ccabf309.png"
+  },
+  {
+    "revision": "dba47083fc3f21f2f81b63d9f7f3699a",
+    "url": "/test/static/media/robotoregular.dba47083.woff2"
+  },
+  {
+    "revision": "088a606660068d25f037ff54bdd97d91",
+    "url": "/test/static/media/robotolight.088a6066.woff2"
+  },
+  {
+    "revision": "7c207019aa62aa4778add0c2b3372720",
+    "url": "/test/static/media/IRANSansWeb_Light.7c207019.woff2"
+  },
+  {
+    "revision": "9d38733b8a7b9153f2bf9bfe476ef8e3",
+    "url": "/test/static/media/IRANSansWeb.9d38733b.woff2"
+  },
+  {
+    "revision": "2f6f164c3e1f1329cc658b3c725bfd98",
+    "url": "/test/static/media/IRANSansWeb_Bold.2f6f164c.woff2"
+  },
+  {
+    "revision": "f9b095429c04a6a7f3bfe25d6c90b21c",
+    "url": "/test/static/media/robotoregular.f9b09542.woff"
+  },
+  {
+    "revision": "ae0cb41c49a1df0fdbc1aaaa016f776d",
+    "url": "/test/static/media/robotolight.ae0cb41c.woff"
+  },
+  {
+    "revision": "50f4bba2f37081daeca8c423abdced8b",
+    "url": "/test/static/media/IRANSansWeb.50f4bba2.woff"
+  },
+  {
+    "revision": "aac7a640c059ee7f46c35c2019434117",
+    "url": "/test/static/media/IRANSansWeb_Light.aac7a640.woff"
+  },
+  {
+    "revision": "d44408d0941f077f60b1306e1331e806",
+    "url": "/test/static/media/IRANSansWeb_Bold.d44408d0.woff"
+  },
+  {
+    "revision": "81ba290a6cef04f8e30b8b7672a5cdba",
+    "url": "/test/static/media/robotobold.81ba290a.woff2"
+  },
+  {
+    "revision": "96b3b238a09b63fbbc09",
+    "url": "/test/static/js/main.96b3b238.chunk.js"
+  },
+  {
+    "revision": "70a5f97c18c45e8ef620a374be61c6c3",
+    "url": "/test/static/media/IRANSansWeb.70a5f97c.eot"
+  },
+  {
+    "revision": "244a401eb43afb0fe739f8dcd8530ae4",
+    "url": "/test/static/media/IRANSansWeb.244a401e.ttf"
+  },
+  {
+    "revision": "9d7efb918cab94704068d84361d3f898",
+    "url": "/test/static/media/IRANSansWeb_Light.9d7efb91.eot"
+  },
+  {
+    "revision": "a65cbf91ec2b253357f7621900ef29a4",
+    "url": "/test/static/media/IRANSansWeb_Bold.a65cbf91.eot"
+  },
+  {
+    "revision": "9471804b0f92127716fabbbb2c6647e5",
+    "url": "/test/static/media/IRANSansWeb_Bold.9471804b.ttf"
+  },
+  {
+    "revision": "e5b2acaef933a97fa3fb026d8ee73d46",
+    "url": "/test/static/media/IRANSansWeb_Light.e5b2acae.ttf"
+  },
+  {
+    "revision": "7b5fb88f12bec8143f00e21bc3222124",
+    "url": "/test/static/media/robotolight.7b5fb88f.ttf"
+  },
+  {
+    "revision": "a28928edbd1711773bad9e1eadfb785f",
+    "url": "/test/static/media/robotoregular.a28928ed.eot"
+  },
+  {
+    "revision": "ac3f799d5bbaf5196fab15ab8de8431c",
+    "url": "/test/static/media/robotoregular.ac3f799d.ttf"
+  },
+  {
+    "revision": "08cf97f4c6e0bdda1a7c548b54c1f0b2",
+    "url": "/test/static/media/robotolight.08cf97f4.eot"
+  },
+  {
+    "revision": "261c61936b688786f2f329e815214c1b",
+    "url": "/test/static/media/robotobold.261c6193.eot"
+  },
+  {
+    "revision": "d329cc8b34667f114a95422aaad1b063",
+    "url": "/test/static/media/robotobold.d329cc8b.ttf"
+  },
+  {
+    "revision": "c1fee5f2fd79b2dd5ef602daefd6fe98",
+    "url": "/test/static/media/news1.c1fee5f2.jpg"
+  },
+  {
+    "revision": "d11126188559a5128ff913040ec85f48",
+    "url": "/test/static/media/news2.d1112618.jpg"
+  },
+  {
+    "revision": "1e8058ceaa71d0c8c19a00483027e9ce",
+    "url": "/test/static/media/news3.1e8058ce.jpg"
+  },
+  {
+    "revision": "e3a92fcd19d1f2b8677c3d239ec2e556",
+    "url": "/test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.da75b96c3845642bc7bef19d81d04d82.js"
+  "/test/precache-manifest.c78315e060dcc32758cdf65a5653ecb2.js"
 );
 
 workbox.clientsClaim();
